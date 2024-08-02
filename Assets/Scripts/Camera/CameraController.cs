565d22e88d28a99156fd822dc04361cb0b4e0bd7
Aleksey <aleksey.vyachislavov@gmail.com>
2024-08-02 18:55:54 +0200

    Camera by drag, sprite fixes, gitignore updated

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,64 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform cameraTransform;
+    public InputActions actions;
+    private InputAction touchPositionAction;
+    private InputAction touchDragAction;
+    private bool touchPressed = false;
 
-    public float movementSpeed;
-    public float movementTime;
+    private Camera mainCamera;
+    public int baseZoom = 1;
+    public int currentZoom;
 
-    public Vector3 newPosition;
+    public float dragSpeed = 1f; // Speed at which the camera moves
 
-    public Vector3 newZoom;
-    public Vector3 zoomAmount;
+    public bool logging = false;
 
-    void Start()
+    void Awake()
     {
-        newPosition = transform.position;
-        newZoom = cameraTransform.localPosition;
+        mainCamera = Camera.main;
+
+        actions = new InputActions();
+
+        // Find the actions from the Input Action Asset
+        touchPositionAction = actions.Touch.Press;
+        touchDragAction = actions.Touch.Drag;
+
+        actions.Touch.Press.performed += ctx =>
+        {
+            touchPressed = true;
+            if(logging) Debug.Log("Touch Enabled");
+        };
+
+        actions.Touch.Press.canceled += ctx =>
+        {
+            touchPressed = false;
+            if(logging) Debug.Log("Touch Released");
+        };
+
+        mainCamera.orthographicSize = baseZoom;
+        currentZoom = baseZoom;
+    }
+
+    private void OnEnable()
+    {
+        // Enable the input actions
+        touchPositionAction.Enable();
+        touchDragAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        // Disable the input actions
+        touchPositionAction.Disable();
+        touchDragAction.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        HandleMovementInput();
+        if (touchPressed)
+        {
+            Vector2 dragDelta = touchDragAction.ReadValue<Vector2>();
+            if (logging && (dragDelta.x > 0.01f || dragDelta.y > 0.01f)) Debug.Log("Drag Delta: " + dragDelta);
+            MoveCamera(dragDelta);
+        }
+
+        mainCamera.orthographicSize = currentZoom;
     }
 
-    void HandleMovementInput()
+    private void MoveCamera(Vector2 dragDelta)
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            newPosition += (transform.forward * movementSpeed);
-        }
+        // Create a movement vector based on the drag delta
+        Vector3 move = new Vector3(-dragDelta.x, -dragDelta.y, 0) * dragSpeed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            newPosition += (transform.forward * -movementSpeed);
-        }
+        // Calculate the rotation needed to align with the camera's 45-degree rotation on the Y axis
+        Quaternion rotation = Quaternion.Euler(0, mainCamera.transform.eulerAngles.y, 0);
 
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            newPosition += (transform.right * movementSpeed);
-        }
+        // Apply the rotation to the movement vector
+        move = rotation * move;
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            newPosition += (transform.right * -movementSpeed);
-        }
-
-        if (Input.GetKey(KeyCode.Q))
-        {
-            newZoom += zoomAmount;
-        }
-
-        if (Input.GetKey(KeyCode.E))
-        {
-            newZoom -= zoomAmount;
-        }
-
-        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
-        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
+        // Apply the movement to the camera's position
+        mainCamera.transform.position += move;
     }
 }
